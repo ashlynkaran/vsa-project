0c7f1b79628894072b9059f31f0bac8efb770441
Ashlyn Karan <ashlynk@gmail.com>
2017-06-19 15:33:28 -0500

    Add files via upload

--- proj01/proj01.py
+++ proj01/proj01.py
@@ -7,4 +7,17 @@
 
 # If you complete extensions, describe your extensions here!
 
+print "Hello world!"
 
+age = int(raw_input("Enter your age: "))
+name = raw_input("Enter your name: ")
+
+if age == 10:
+    print "you're 10"
+    if name == "Matthew":
+        print "Matthew's great!"
+
+elif age == 20:
+    print "you're 20"
+else:
+    print "you're not 10 or 20."
